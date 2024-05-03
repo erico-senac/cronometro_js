296db8a00624160f20892d622635246a3f47cefb
erico-senac <professorerico@pi.senac.br>
2024-05-02 21:56:05 -0300

    controle de pausas

--- asset/js/cronometro.js
+++ asset/js/cronometro.js
@@ -3,17 +3,31 @@ const div_hora = document.querySelector(".cronometro");
 const input_radio = document.querySelectorAll("input[name*=cronometro]");
 const inputs_tempo = document.querySelectorAll('input[name=def_time]');
 const btn_start = document.querySelector('#botao_start');
+const div_acoes_crono = document.querySelector('#acoes_crono');
+let cronoId = null;
+let tempo;
+let data;
 
-const pararContagem = () => {
-    clearInterval(contador);
-    btn_start.addEventListener('click', iniciarContagem);
-}
-
-const iniciarContagem = () => {
+const startPause = () => {
     btn_start.querySelector('i').classList.toggle('fa-play');
     btn_start.querySelector('i').classList.toggle('fa-stop');
-    btn_start.addEventListener('click', pararContagem);
-
+    if(cronoId){
+        clearInterval(tempo);
+        cronoId = null;
+        return;
+    }
+    iniciarContagem();
+}
+const exibeTempo = (tempoAExibir) => {
+    div_hora.innerHTML =tempoAExibir.toLocaleString('pt-BR',
+                {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                }
+            );
+}
+const iniciarContagem = () => {
     let hora = parseInt(document.querySelector('[hora]').value);
     let minuto = parseInt(document.querySelector('[minuto]').value);
     let segundo = parseInt(document.querySelector('[segundo]').value);
@@ -22,20 +36,15 @@ const iniciarContagem = () => {
     const agora = new Date(d.getFullYear(),d.getMonth(),d.getDate(),0,0,0);
     let milisegundos = Date.parse(agora);
     tempo_total += milisegundos;
-    const contador = setInterval(() => {
+    tempo = setInterval(() => {
+        cronoId = tempo_total;
         if(tempo_total - milisegundos == 0){
-            clearInterval(contador);
-            btn_start.querySelector('i').classList.toggle('fa-play');
-            btn_start.querySelector('i').classList.toggle('fa-stop');
+            clearInterval(tempo);
+            cronoId = null;
+            startPause();
         }
         let tempo_crono = new Date(tempo_total);
-        div_hora.innerHTML = tempo_crono.toLocaleString('pt-BR',
-                {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    second: '2-digit',
-                }
-            );
+        exibeTempo(tempo_crono);
         let tipo = document.querySelector('[name="tipo_cronometro"]:checked').value;
         if(tipo === "Prog")
             tempo_total+=1000;
@@ -44,19 +53,16 @@ const iniciarContagem = () => {
     }, 1000);
 }
 
-
 input_radio.forEach(el => {
     el.addEventListener('click', () => {
+        clearInterval(tempo);
         if(el.value !== 'Hora'){
-            clearInterval(data_hora);
-            document.querySelector('.def_time').classList.remove('hide');
+            div_acoes_crono.classList.remove('hide');
             div_hora.innerHTML = "";
-            let tipo = el.value
-            btn_start.addEventListener('click', iniciarContagem);
+            btn_start.addEventListener('click', startPause);
         } else {
-            document.querySelector('.def_time').classList.add('hide');
             iniciaHora();
-            btn_start.setAttribute('enable',false);
+            div_acoes_crono.classList.add('hide');
         }
     });
 });
@@ -87,9 +93,20 @@ const diaDaSemana = (dow) =>{
     }
 }
 
-let data_hora;
 const iniciaHora = () => {
-    data_hora = setInterval(() => {
+    tempo = setInterval(() => {
+        /* captura a data */
+        let tempo = new Date();
+        /* fragmenta a hora */
+        let hora = tempo.getHours().toString().padStart(2,'0');
+        let minuto = tempo.getMinutes().toString().padStart(2,'0');
+        let segundos = tempo.getSeconds().toString().padStart(2,'0');
+        /* exibe a hora */
+        div_hora.innerHTML = `${hora}:${minuto}:${segundos}`;
+    }, 1000);
+}
+const iniciaData = () => {
+    data = setInterval(() => {
         /* captura a data */
         let tempo = new Date();
         /* fragmenta a data */
@@ -97,14 +114,9 @@ const iniciaHora = () => {
         let dia = tempo.getDate().toString().padStart(2,'0');
         let mes = (tempo.getMonth() + 1).toString().padStart(2,'0');
         let ano = tempo.getFullYear().toString();
-        /* fragmenta a hora */
-        let hora = tempo.getHours().toString().padStart(2,'0');
-        let minuto = tempo.getMinutes().toString().padStart(2,'0');
-        let segundos = tempo.getSeconds().toString().padStart(2,'0');
-        let milisegundos = tempo.getMilliseconds().toString().padStart(3,'0');
-        /* exibe a hora */
-        div_hora.innerHTML = `${hora}:${minuto}:${segundos}`;
+        /* exibe a data */
         div_data.innerHTML = `${diaDaSemana(dow)} - ${dia}/${mes}/${ano}`;
     }, 1000);
 }
 iniciaHora();
+// iniciaData();
